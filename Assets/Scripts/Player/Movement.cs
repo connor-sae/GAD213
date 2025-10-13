0012b4a789ceb2a74f492bd644129f8c9186935e
connor-sae <connor.westhuizen@student.saedubai.com>
2025-10-13 18:00:43 +0400

    Added Animation Curves to movement interpolation

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,5 +1,6 @@
 using System;
 using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,7 +10,21 @@ namespace Player
     public class Movement : MonoBehaviour
     {
         public float gridSize = 5f;
+
+        public float vAccel = 9.8f;
+        bool grounded;
+        float fallVelocity = 0;
+
+        public float moveDelay = 0.5f;
+        public AnimationCurve MoveInterpolation;
+        float lastMoveTime = 0f;
+        [SerializeField] private float inputBufferTime = 0.05f;
+        short inputBuffered;
+
+        [Space]
         [SerializeField] private float playerRadius = 1.5f;
+        [SerializeField] private float playerHeight = 4.0f;
+        [SerializeField] private float slopeLenience = 0.2f;
 
         [Space]
         [SerializeField] private Transform moveCastPoint;
@@ -19,6 +34,9 @@ namespace Player
 
         [Space]
         [SerializeField] private string ElevatorTag = "Elevator";
+
+        Vector2 targetPosition;
+        Vector2 lastPosition;
 
 
         private void OnEnable()
@@ -33,32 +51,72 @@ namespace Player
             InputManager.OnPlayerRotate -= OnRotate;
         }
 
-        bool grounded;
-        private void Update()
+        void Start()
         {
-            Collider[] hits = Physics.OverlapSphere(transform.position, groundOverlapRadius, groundLayer);
-            Debug.Log(hits.Length);
-            grounded = (hits.Length > 0);
-
+            //targetPosition = transform.position;
+            //last
         }
 
+        private void Update()
+        {
+
+            //// Check if Player is Grounded and snap put on Ground
+            
+            grounded = Physics.SphereCast(transform.position + new Vector3(0, playerHeight, 0), groundOverlapRadius, Vector3.down,
+             out RaycastHit hit, playerHeight + (grounded ? slopeLenience : 0f), groundLayer);
+
+            if (grounded)
+            {
+                fallVelocity = 0;
+                transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z);
+            }
+            else
+            {
+                fallVelocity += vAccel * Time.deltaTime;
+                transform.position += new Vector3(0, -fallVelocity, 0);
+            }
+
+            //move at the end if input buffered
+            if ( inputBuffered != 0 && Time.time - moveDelay > lastMoveTime)
+                    OnMove(inputBuffered);
+
+            //move player to target position
+
+            Vector2 erp = targetPosition - lastPosition;
+            erp *= MoveInterpolation.Evaluate((Time.time - lastMoveTime) / moveDelay);
+            transform.position = SwizzleFromXY(erp + lastPosition, transform.position.y);
+        }
 
         public void OnMove(short dir)
         {
-            RaycastHit hit;
+
+            if (Time.time - moveDelay < lastMoveTime)
+            {
+                if (Time.time - moveDelay - inputBufferTime > lastMoveTime)
+                    inputBuffered = dir;
+                return;
+            }
+            ;
             if (!grounded) return;
-            if(moveCastPoint == null)
+
+            if (moveCastPoint == null)
             {
                 Debug.LogError("Move Cast Point Undefined");
                 return;
             }
+
+            RaycastHit hit;
             if (Physics.SphereCast(moveCastPoint.position, moveCastRadius, transform.forward * dir, out hit, gridSize + playerRadius))
             {
                 if (!hit.collider.CompareTag(ElevatorTag))
                     return;
             }
+            //ignore y component
+            lastPosition = targetPosition;
+            targetPosition += SwizzleToXZ(transform.forward) * dir * gridSize;
+            lastMoveTime = Time.time;
+            inputBuffered = 0;
 
-            transform.position += transform.forward * dir * gridSize;
         }
 
         private void OnRotate(short dir)
@@ -78,16 +136,27 @@ namespace Player
             Gizmos.DrawRay(moveCastPoint.position, transform.forward * -1 * (gridSize + playerRadius));
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(moveCastPoint.position + transform.forward * -1 * (gridSize + playerRadius), moveCastRadius);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(moveCastPoint.position, transform.position);
+            Gizmos.color = Color.red;
             Gizmos.DrawSphere(transform.position, 0.3f);
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position + new Vector3(0, playerRadius, 0), playerRadius);
+            Gizmos.DrawWireSphere(transform.position + new Vector3(0, playerHeight - playerRadius, 0), playerRadius);
+
+            Gizmos.color = Color.greenYellow;
+            Gizmos.DrawSphere(new Vector3(targetPosition.x, transform.position.y, targetPosition.y), 1f);
         }
 
-        public enum Direction
+        private Vector2 SwizzleToXZ(Vector3 vec3)
         {
-            FORWARD,
-            BACKWARD,
-            RIGHT,
-            LEFT,
+            return new Vector2(vec3.x, vec3.z);
         }
 
+        private Vector3 SwizzleFromXY(Vector2 vec2, float yVal = 0)
+        {
+            return new Vector3(vec2.x, yVal, vec2.y);
+        }
     }
 }
